8feb9d7952c006408b088a80a9f48a03d5d1770b
andrej22116 <andrej22116@hotmail.com>
2018-10-02 20:04:33 +0300

    Lr_5 66% - complate task 2

--- PDI/Lr_5/script.js
+++ PDI/Lr_5/script.js
@@ -1,24 +1,64 @@
-function main() {
-    var inputs = document.getElementsByTagName("input");
-    for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].dataset.hint == undefined) continue;
-
-        var newElement = document.createElement('label');
-        newElement.innerHTML = '<b>?</b>';
-        newElement.setAttribute('title', inputs[i].dataset.hint);
-        newElement.style.borderWidth = '1';
-        newElement.style.borderColor = 'green';
-        newElement.style.borderStyle = 'solid';
-        newElement.style.fontSize = '14px';
-        newElement.style.padding = "0 5px 0 5px";
-
-        if (inputs[i].nextSibling == undefined) {
-            inputs[i].parentNode.appendChild(newElement);
-        }
-        else {
-            inputs[i].parentNode.insertBefore(newElement, inputs[i].nextSibling);
-        }        
+function insertAfter(newNode, afterElement) {
+    if (afterElement.nextElementSibling == null) {
+        afterElement.parentElement.appendChild(newNode);
+    }
+    else {
+        afterElement.parentElement.insertBefore(newNode, afterElement.nextElementSibling);
     }
 }
 
-main();
+function setErrorMessage(input, message) {
+    var errorElement = document.createElement('label');
+    errorElement.style.color = 'red';
+    errorElement.style.border = '1px solid red';
+    errorElement.style.padding = "2px 10px 2px 10px";
+    errorElement.style.margin = "0 10px 0 0";
+    errorElement.innerHTML = message;
+    errorElement.setAttribute('error-message', '!');
+    
+    insertAfter(errorElement, input);
+
+    return false;
+}
+
+function removeOldErrorMessage(input) {
+    if(input.nextElementSibling != null 
+        && input.nextElementSibling.getAttribute('error-message') == '!') {
+        input.parentElement.removeChild(input.nextElementSibling);
+    }
+}
+
+function validateIntegerInput(input) {
+    removeOldErrorMessage(input);
+
+    if (input.value == '' || input.value.split(/[^(-?\d*)]/).length > 1) { 
+        return setErrorMessage(input, "Введите число!"); 
+    }
+
+    if (input.getAttribute('minrange') != null && +input.value < +input.getAttribute('minrange')) {
+        return setErrorMessage(input, "Число должно быть больше или равно " + input.getAttribute('minrange') + "!");
+    }
+    if (input.getAttribute('maxrange') != null && +input.value > +input.getAttribute('maxrange')) {
+        return setErrorMessage(input, "Число должно быть меньше или равно " + input.getAttribute('maxrange') + "!");
+    }
+
+    return true;
+}
+
+function validate(form)
+{
+    var childElemets = form.getElementsByTagName('input');
+    var dataIsAcceptable = true;
+
+    for (var i = 0; i < childElemets.length; i++) {
+        if (childElemets[i].getAttribute('extype') == 'integer') {
+            if (!validateIntegerInput(childElemets[i])) {
+                dataIsAcceptable = false;
+            }
+        }
+    }
+
+    if (!dataIsAcceptable) {
+        event.preventDefault(); 
+    }
+}
